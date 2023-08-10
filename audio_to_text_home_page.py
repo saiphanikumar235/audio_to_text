af42c9b5464e66b7881f75f9e7cc5cfb97c26c41
saiphanikumar235 <138872112+saiphanikumar235@users.noreply.github.com>
2023-08-10 16:55:09 +0530

    Update audio_to_text_home_page.py

--- audio_to_text_home_page.py
+++ audio_to_text_home_page.py
@@ -4,13 +4,13 @@ import openai
 
 
 def auido_to_text(audio):
-    openai.api_key = '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'
+    openai.api_key = ''
     result = openai.Audio.transcribe("whisper-1", audio, verbose=True)
     return result
 
 
 def video_to_text(video):
-    openai.api_key = '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'
+    openai.api_key = ''
     result = openai.Audio.transcribe("whisper-1", video, verbose=True)
     return result
 
