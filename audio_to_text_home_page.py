b67788473e0761a20f33ee48ab692c7977f74747
saiphanikumar235 <138872112+saiphanikumar235@users.noreply.github.com>
2023-08-10 16:32:28 +0530

    Update audio_to_text_home_page.py

--- audio_to_text_home_page.py
+++ audio_to_text_home_page.py
@@ -1,6 +1,19 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
-import speech_to_text as local_file
+import openai
+
+
+def auido_to_text(audio):
+    openai.api_key = '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'
+    result = openai.Audio.transcribe("whisper-1", audio, verbose=True)
+    return result
+
+
+def video_to_text(video):
+    openai.api_key = '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'
+    result = openai.Audio.transcribe("whisper-1", video, verbose=True)
+    return result
+
 
 selected = option_menu(
     menu_title=None,
@@ -16,13 +29,13 @@ elif selected == 'Audio':
     audio = st.file_uploader("Upload an audio file", type=["mp3"])
     if audio is not None:
         with st.spinner("Converting audio to speech...."):
-            result = local_file.auido_to_text(audio)
+            result = auido_to_text(audio)
         with st.container():
             st.write(result['text'])
 elif selected == 'Video':
     video = st.file_uploader("Upload video file", type=['mp4'])
     if video is not None:
         with st.spinner("Converting video to speech...."):
-            result = local_file.video_to_text(video)
+            result = video_to_text(video)
         with st.container():
             st.write(result['text'])
